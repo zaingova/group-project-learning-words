4b5bf3ca542420170e36a9f6321627fdad4ea535
Gabriel Parada <gabrielparada05@gmail.com>
2023-05-17 10:59:05 -0400

    Free Dic API working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,12 @@ var inputSearch = document.querySelector("#wordEnter");
 var inputBtn = document.querySelector("#submitBtn");
 var searchResult = document.querySelector("#searchResult")
 var searchHistory = document.querySelector("#searchHistory");
+var ulSearch = document.querySelector('#ulSearch');
+var wordInput = document.querySelector('#word');
+
 
 var apiCallFree = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+var apiCallWord = 'https://wordsapiv1.p.mashape.com/words/';
 
 window.onload = function () {
     inputSearch.addEventListener("keypress", function (event) {
@@ -13,7 +17,7 @@ window.onload = function () {
     })
 }
 
-/*var searchHistoryArray;
+
 if (localStorage.getItem("wordSearch")) {
     searchHistoryArray = JSON.parse(localStorage.getItem("wordSearch"));
     var newArr = searchHistoryArray.slice(0, 7);
@@ -30,36 +34,31 @@ if (localStorage.getItem("wordSearch")) {
 } else {
     searchHistoryArray = [];
 }
-*/
+
 function formSubmitHandler(event) {
     event.preventDefault();
-
     var word = inputSearch.value;
-
     if (word) {
         getWord(word);
         inputSearch.value = "";
     } else {
         alert("Please enter a word");
     }
-}
 
 
-function getWord(word, addToLocalStorage = true) {
+    function getWord(word, addToLocalStorage = true) {
     var apiUrl = apiCallFree + word;
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
                     var wordComplete = data[0].word;
-                    var wordInput = document.createElement("h3");
                     wordInput.textContent = wordComplete;
-                    searchResult.appendChild(wordInput);
                     console.log(data)
+                    displayWord(data)
                     if (addToLocalStorage) {
                         searchHistoryArray.unshift(wordComplete);
                         localStorage.setItem("wordSearch", JSON.stringify(searchHistoryArray));
-
                         var listBtn2 = document.createElement("li");
                         var newBtn2 = document.createElement("button");
                         newBtn2.textContent = wordComplete;
@@ -78,73 +77,64 @@ function getWord(word, addToLocalStorage = true) {
             alert("Unable to connect to Free Dictionary");
         });
 
-<<<<<<< HEAD:assets/js/script.js
-    }
-// Display word 
-/*
-var displayWord = function (content) {
-    var temperatureEl = document.querySelector("#temperature");
-    var descriptionEl = document.querySelector("#description");
-    var humidityEl = document.querySelector("#humidity");
-   
-    var temperaturePrint = content.main.temp;
-    var descriptionPrint = content.weather[0].main;
-    var humidityPrint = content.main.humidity;
-    outputSearch.setAttribute("style", "border:rgb(180, 215, 180) dashed 2px!important; border-radius:4px; align-items:center; padding:0px 5px 15px 5px");
-    temperatureEl.textContent = "Temperature: " + temperaturePrint + " °C";
-    descriptionEl.textContent = "Weather: " + descriptionPrint;
-    humidityEl.textContent = "Humidity: " + humidityPrint + " %";
-   
-   
-    getForecast(content.name)
-  }
+    
 
-
-
-
-
-
-
-
-
-=======
-
-    // Display word 
-    var displayWord = function (content) {
-        var temperatureEl = document.querySelector("#temperature");
-        var descriptionEl = document.querySelector("#description");
-        var humidityEl = document.querySelector("#humidity");
-
-        var temperaturePrint = content.main.temp;
-        var descriptionPrint = content.weather[0].main;
-        var humidityPrint = content.main.humidity;
-        outputSearch.setAttribute("style", "border:rgb(180, 215, 180) dashed 2px!important; border-radius:4px; align-items:center; padding:0px 5px 15px 5px");
-        temperatureEl.textContent = "Temperature: " + temperaturePrint + " °C";
-        descriptionEl.textContent = "Weather: " + descriptionPrint;
-        humidityEl.textContent = "Humidity: " + humidityPrint + " %";
->>>>>>> 9c8e84b25843e5c14c196a3c4a69a9e68f9a1f45:assets/js/style.js
-
-
-        getForecast(content.name)
+        var displayWord = function (data) {
+        var wordSound = document.querySelector('#sound');
+        var wordDef = document.querySelector('#definition');
+        var wordEx = document.querySelector('#example');
+        var wordType = document.querySelector('#wordType');
+        var definitionPrint = data[0].meanings[0].definitions[0].definition;
+        var soundPrint = data[0].phonetics[1].sourceUrl;
+        /// var examplePrint = data[0].meaning[0].definition[0];
+        var typePrint = data[0].meanings[0].partOfSpeech;
+        wordDef.textContent = "Meanings: " + definitionPrint;
+        wordSound.textContent = "Phonetic: " + soundPrint;
+        wordType.textContent = 'Part of speech: ' + typePrint; 
+    
+        //getNextApi(data.name)
+        }
     }
 
 
+  //get Next API
+  var getNextApi = function (word) {
+    var apiUrl2 = apiCallWord + word;
+    fetch(apiUrl2)
+      .then(function (response1) {
+        if (response1.ok) {
+          response1.json().then(function (dataFor) {
+            displaySynon(dataFor);
+          });
+        } else {
+          alert("Error: " + response1.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert("Unable to connect to WordAPI");
+      });
+
+
+      //Display synonyms 
+      var displaySynon = function (dataFor) {
+        var day1 = document.querySelector("#day-1");
+        
+  
+        forecastTitle.textContent = "5-Day Forecast"
+  
+        var day1Temp = dataFor.list[0].main.temp;
+        var day1x = dataFor.list[0].weather[0].icon;
+        var day1Desc = "http://openweathermap.org/img/w/" + day1x + ".png";
+        var day1Wind = dataFor.list[0].wind.speed;
+        var day1Date = dataFor.list[0].dt_txt;
+        var day1Humidity = dataFor.list[0].main.humidity;
+
+
+      }
 
 
 
+        //getForecast(content.name)
+}}
 
-
-
-
-
-
-}
-<<<<<<< HEAD:assets/js/script.js
-*/
 inputBtn.addEventListener("click", formSubmitHandler);
-=======
-
-inputBtn.addEventListener("click", formSubmitHandler);
-
-
->>>>>>> 9c8e84b25843e5c14c196a3c4a69a9e68f9a1f45:assets/js/style.js
