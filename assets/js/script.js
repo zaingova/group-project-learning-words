ab6173148f2eb2e18c078903b4af956e3fb298f4
zaingova <zain.gova.school@gmail.com>
2023-05-17 17:38:29 -0400

    worked on the audio src set in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,12 +88,13 @@ function formSubmitHandler(event) {
         var wordEx = document.querySelector('#example');
         var wordType = document.querySelector('#wordType');
         var definitionPrint = data[0].meanings[0].definitions[0].definition;
-        var soundPrint = data[0].phonetics[1].sourceUrl;
         /// var examplePrint = data[0].meaning[0].definition[0];
         var typePrint = data[0].meanings[0].partOfSpeech;
         wordDef.textContent = "Meanings: " + definitionPrint;
-        wordSound.textContent = "Phonetic: " + soundPrint;
-        wordType.textContent = 'Part of speech: ' + typePrint; 
+        //wordSound.textContent = "Phonetic: " + soundPrint;
+        //wordSound = "https://api.dictionaryapi.dev/media/pronunciations/en/cat-us.mp3";
+        wordType.textContent = 'Part of speech: ' + typePrint;
+        wordSound.setAttribute("src", data[0].phonetics[1].audio);
     
         getNextApi(data[0].word)
         }
