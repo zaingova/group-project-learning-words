3cc4da320d6ee280e2d971938404b22133303dfb
Gabriel Parada <gabrielparada05@gmail.com>
2023-05-17 17:30:54 -0400

    JS working. Only bug the button which created after search, is not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Set the variables in order to select the HTML Element 
 var inputSearch = document.querySelector("#wordEnter");
 var inputBtn = document.querySelector("#submitBtn");
 var searchResult = document.querySelector("#searchResult")
@@ -6,38 +7,43 @@ var ulSearch = document.querySelector('#ulSearch');
 var wordInput = document.querySelector('#word');
 var mainPanel = document.querySelector('#mainPanel');
 
-
+// API variables
 var apiCallFree = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 var apiCallWord = 'https://wordsapiv1.p.mashape.com/words/';
-var synonyms = '/synonyms'
+var synonyms = '/synonyms';
 
-
-    inputSearch.addEventListener("keypress", function (event) {
-        if (event.keyCode === 13) {
-            formSubmitHandler(event)
-        }
-    })
-
-
-    if (localStorage.getItem("wordSearch")) {
-        searchHistoryArray = JSON.parse(localStorage.getItem("wordSearch"));
-        var newArr = searchHistoryArray.slice(0, 7);
-        newArr.forEach(word => {
-            searchHistory.setAttribute("style","display:block");
-            var listBtn = document.createElement("li");
-            var newBtn = document.createElement("button");
-            newBtn.textContent = word;
-            listBtn.appendChild(newBtn);
-            ulSearch.appendChild(listBtn);
-            newBtn.onclick = function () {
-                getWord(word, false);
-            }
-        });
-    } else {
-        searchHistoryArray = [];
+  /// Keypress event in search bar
+inputSearch.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+        formSubmitHandler(event);
     }
+});
 
 
+  // Get search history from localStorage
+var searchHistoryArray;
+
+if (localStorage.getItem("wordSearch")) {
+    searchHistoryArray = JSON.parse(localStorage.getItem("wordSearch"));
+    var newArr = searchHistoryArray.slice(0, 9);
+    newArr.forEach(word => {
+        searchHistory.setAttribute("style", "display:block");
+        var listBtn = document.createElement("li");
+        var newBtn = document.createElement("button");
+        newBtn.textContent = word;
+        listBtn.appendChild(newBtn);
+        ulSearch.appendChild(listBtn);
+        // Do a fetch on the buttons displayed but does not include again in the localStorage
+        newBtn.onclick = function () {
+            getWord(word, false);
+        };
+    });
+} else {
+    searchHistoryArray = [];
+}
+
+
+  /// Principal trigger 
 function formSubmitHandler(event) {
     event.preventDefault();
     var word = inputSearch.value;
@@ -47,9 +53,9 @@ function formSubmitHandler(event) {
     } else {
         alert("Please enter a word");
     }
-
-
-    function getWord(word, addToLocalStorage = true) {
+}
+ // Fetch current word
+function getWord(word, addToLocalStorage = true) {
     var apiUrl = apiCallFree + word;
     fetch(apiUrl)
         .then(function (response) {
@@ -58,27 +64,27 @@ function formSubmitHandler(event) {
                     var wordComplete = data[0].word;
                     mainPanel.style.display = 'block';
                     wordInput.textContent = wordComplete;
-                    console.log(data)
-                    displayWord(data)
+                    console.log(data);
+                    displayWord(data);
                     if (addToLocalStorage) {
+                        // Add word to search history array and save to localStorage
                         searchHistoryArray.unshift(wordComplete);
                         localStorage.setItem("wordSearch", JSON.stringify(searchHistoryArray));
-                        var listBtn2 = document.createElement("li");
-                        var newBtn2 = document.createElement("button");
-                        newBtn2.textContent = word;
-                        listBtn2.appendChild(newBtn2);
-                        ulSearch.appendChild(listBtn2);
-                        
-                        
+                         // Add word to search history list
                         var newBtn2 = document.createElement("button");
                         newBtn2.textContent = wordComplete;
                         ulSearch.appendChild(newBtn2);
-                        if (ulSearch.children.length > 7) {
-                            ulSearch.removeChild(ulSearch.lastChild);
+                             // Only show 10 elements in  history-search
+                        if (searchHistory.children.length > 9) {
+                            searchHistory.removeChild(searchHistory.lastChild);
                         }
+                        var listBtn = document.createElement("li");
                         searchHistory.prepend(listBtn);
+                        newBtn2.onclick = function () {
+                            getWord(word, false);
+                        };
                     }
-                })
+                });
             } else {
                 alert("Error: " + response.statusText);
             }
@@ -86,66 +92,65 @@ function formSubmitHandler(event) {
         .catch(function (error) {
             alert("Unable to connect to Free Dictionary");
         });
+}
 
-    
+// Display word 
 
-        var displayWord = function (data) {
-        var wordSound = document.querySelector('#sound');
-        var wordDef = document.querySelector('#definition');
-        var wordEx = document.querySelector('#example');
-        var wordType = document.querySelector('#wordType');
-        var definitionPrint = data[0].meanings[0].definitions[0].definition;
-        var soundPrint = data[0].phonetics[1].sourceUrl;
-        /// var examplePrint = data[0].meaning[0].definition[0];
-        var typePrint = data[0].meanings[0].partOfSpeech;
-        wordDef.textContent = "Meanings: " + definitionPrint;
-        wordSound.textContent = "Phonetic: " + soundPrint;
-        wordType.textContent = 'Part of speech: ' + typePrint; 
-    
-        //getNextApi(data[0].word)
-        }
+function displayWord(data) {
+    var wordSound = document.querySelector('#sound');
+    var wordDef = document.querySelector('#definition');
+    var wordEx = document.querySelector('#example');
+    var wordType = document.querySelector('#wordType');
+    var definitionPrint = data[0].meanings[0].definitions[0].definition;
+    var soundPrint = data[0].phonetics[1].sourceUrl;
+    var typePrint = data[0].meanings[0].partOfSpeech;
+    wordDef.textContent = "Meanings: " + definitionPrint;
+    wordSound.textContent = "Phonetic: " + soundPrint;
+    wordType.textContent = 'Part of speech: ' + typePrint;
+}
+
+//Trigger
+inputBtn.addEventListener("click", formSubmitHandler);
+
+
+
+
+/*get Next API
+var getNextApi = function (word) {
+  var apiUrl2 = apiCallWord + word;
+  fetch(apiUrl2)
+    .then(function (response1) {
+      console.log(response1)
+      if (response1.ok) {
+        response1.json().then(function (dataFor) {
+          displaySynon(dataFor);
+        });
+      } else {
+        alert("Error: " + response1.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to Word Api");
+    });
+
+
+    //Display synonyms 
+    var displaySynon = function (dataFor) {
+      var day1 = document.querySelector("#day-1");
+      
+ 
+      forecastTitle.textContent = "5-Day Forecast"
+ 
+      var day1Temp = dataFor.list[0].main.temp;
+      var day1x = dataFor.list[0].weather[0].icon;
+      var day1Desc = "http://openweathermap.org/img/w/" + day1x + ".png";
+      var day1Wind = dataFor.list[0].wind.speed;
+      var day1Date = dataFor.list[0].dt_txt;
+      var day1Humidity = dataFor.list[0].main.humidity;
+
+
     }
 
 
-  /*get Next API
-  var getNextApi = function (word) {
-    var apiUrl2 = apiCallWord + word;
-    fetch(apiUrl2)
-      .then(function (response1) {
-        console.log(response1)
-        if (response1.ok) {
-          response1.json().then(function (dataFor) {
-            displaySynon(dataFor);
-          });
-        } else {
-          alert("Error: " + response1.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert("Unable to connect to Word Api");
-      });
 
-
-      //Display synonyms 
-      var displaySynon = function (dataFor) {
-        var day1 = document.querySelector("#day-1");
-        
-  
-        forecastTitle.textContent = "5-Day Forecast"
-  
-        var day1Temp = dataFor.list[0].main.temp;
-        var day1x = dataFor.list[0].weather[0].icon;
-        var day1Desc = "http://openweathermap.org/img/w/" + day1x + ".png";
-        var day1Wind = dataFor.list[0].wind.speed;
-        var day1Date = dataFor.list[0].dt_txt;
-        var day1Humidity = dataFor.list[0].main.humidity;
-
-
-      }
-
-
-
-        //getForecast(content.name)}*/
-
-}
-inputBtn.addEventListener("click", formSubmitHandler);
+      //getForecast(content.name)}*/
