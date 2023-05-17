80eff288282b8c411776567345ae53f3f4cac716
zaingova <zain.gova.school@gmail.com>
2023-05-17 10:48:14 -0400

    added dev background colors so that we can see a clear division of sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,6 +56,7 @@ function getWord(word, addToLocalStorage = true) {
                     wordInput.textContent = wordComplete;
                     searchResult.appendChild(wordInput);
                     console.log(data)
+                    displayWord
                     if (addToLocalStorage) {
                         searchHistoryArray.unshift(wordComplete);
                         localStorage.setItem("wordSearch", JSON.stringify(searchHistoryArray));
@@ -78,7 +79,6 @@ function getWord(word, addToLocalStorage = true) {
             alert("Unable to connect to Free Dictionary");
         });
 
-<<<<<<< HEAD:assets/js/script.js
     }
 // Display word 
 /*
@@ -107,22 +107,6 @@ var displayWord = function (content) {
 
 
 
-=======
-
-    // Display word 
-    var displayWord = function (content) {
-        var temperatureEl = document.querySelector("#temperature");
-        var descriptionEl = document.querySelector("#description");
-        var humidityEl = document.querySelector("#humidity");
-
-        var temperaturePrint = content.main.temp;
-        var descriptionPrint = content.weather[0].main;
-        var humidityPrint = content.main.humidity;
-        outputSearch.setAttribute("style", "border:rgb(180, 215, 180) dashed 2px!important; border-radius:4px; align-items:center; padding:0px 5px 15px 5px");
-        temperatureEl.textContent = "Temperature: " + temperaturePrint + " °C";
-        descriptionEl.textContent = "Weather: " + descriptionPrint;
-        humidityEl.textContent = "Humidity: " + humidityPrint + " %";
->>>>>>> 9c8e84b25843e5c14c196a3c4a69a9e68f9a1f45:assets/js/style.js
 
 
         getForecast(content.name)
@@ -139,12 +123,5 @@ var displayWord = function (content) {
 
 
 }
-<<<<<<< HEAD:assets/js/script.js
 */
 inputBtn.addEventListener("click", formSubmitHandler);
-=======
-
-inputBtn.addEventListener("click", formSubmitHandler);
-
-
->>>>>>> 9c8e84b25843e5c14c196a3c4a69a9e68f9a1f45:assets/js/style.js
